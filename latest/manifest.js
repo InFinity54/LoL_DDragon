49ebe43187d270616b48d5fbb51105d33078ee1d
InFinity_54 <corentinkreicher@gmail.com>
2020-07-01 15:26:57 +0200

    Added patch 7.2.1

--- latest/manifest.js
+++ latest/manifest.js
@@ -1,1 +1,1 @@
-Riot.DDragon.m={"n":{"item":"7.1.1","rune":"7.1.1","mastery":"7.1.1","summoner":"7.1.1","champion":"7.1.1","profileicon":"7.1.1","map":"7.1.1","language":"7.1.1"},"v":"7.1.1","l":"en_US","cdn":null,"dd":"7.1.1","lg":"7.1.1","css":"7.1.1","profileiconmax":28};
+Riot.DDragon.m={"n":{"item":"7.2.1","rune":"7.2.1","mastery":"7.2.1","summoner":"7.2.1","champion":"7.2.1","profileicon":"7.2.1","map":"7.2.1","language":"7.2.1"},"v":"7.2.1","l":"en_US","cdn":null,"dd":"7.2.1","lg":"7.2.1","css":"7.2.1","profileiconmax":28};
