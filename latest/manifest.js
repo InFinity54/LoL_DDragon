252a01901bf139894729d10819e8a31c2ab35d3c
InFinity_54 <corentinkreicher@gmail.com>
2020-07-01 13:00:38 +0200

    Added patch 4.8.2

--- latest/manifest.js
+++ latest/manifest.js
@@ -1,1 +1,1 @@
-Riot.DDragon.m={"n":{"item":"4.8.1","rune":"4.8.1","mastery":"4.8.1","summoner":"4.8.1","champion":"4.8.1","profileicon":"4.8.1","language":"4.8.1"},"v":"4.8.1","l":"en_US","cdn":"http://ddragon.leagueoflegends.com/cdn","dd":"4.2.6","lg":"0.152.55","css":"0.152.55","profileiconmax":28};
+Riot.DDragon.m={"n":{"item":"4.8.2","rune":"4.8.2","mastery":"4.8.2","summoner":"4.8.2","champion":"4.8.2","profileicon":"4.8.2","language":"4.8.2"},"v":"4.8.2","l":"en_US","cdn":"http://ddragon.leagueoflegends.com/cdn","dd":"4.2.6","lg":"0.152.55","css":"0.152.55","profileiconmax":28};
