e94e7e8c9856a4929b480f40359a71bac3d58120
InFinity_54 <corentinkreicher@gmail.com>
2020-07-02 12:32:02 +0200

    Added patch 8.15.1

--- lolpatch_latest/manifest.js
+++ lolpatch_latest/manifest.js
@@ -1,1 +1,1 @@
-Riot.DDragon.m={"n":{"item":"8.14.1","rune":"8.14.1","mastery":"8.14.1","summoner":"8.14.1","champion":"8.14.1","profileicon":"8.14.1","map":"8.14.1","language":"8.14.1","sticker":"8.14.1"},"v":"8.14.1","l":"en_US","cdn":null,"dd":"8.14.1","lg":"8.14.1","css":"8.14.1","profileiconmax":28};
+Riot.DDragon.m={"n":{"item":"8.15.1","rune":"8.15.1","mastery":"8.15.1","summoner":"8.15.1","champion":"8.15.1","profileicon":"8.15.1","map":"8.15.1","language":"8.15.1","sticker":"8.15.1"},"v":"8.15.1","l":"en_US","cdn":null,"dd":"8.15.1","lg":"8.15.1","css":"8.15.1","profileiconmax":28};
