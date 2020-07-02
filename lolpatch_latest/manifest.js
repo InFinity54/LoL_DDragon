82f6782e2a34317034194846118dbe2bbf222b8b
InFinity_54 <corentinkreicher@gmail.com>
2020-07-02 14:37:46 +0200

    Added patch 10.5.1

--- lolpatch_latest/manifest.js
+++ lolpatch_latest/manifest.js
@@ -1,1 +1,1 @@
-Riot.DDragon.m={"n":{"item":"10.4.1","rune":"10.4.1","mastery":"10.4.1","summoner":"10.4.1","champion":"10.4.1","profileicon":"10.4.1","map":"10.4.1","language":"10.4.1","sticker":"10.4.1"},"v":"10.4.1","l":"en_US","cdn":null,"dd":"10.4.1","lg":"10.4.1","css":"10.4.1","profileiconmax":28};
+Riot.DDragon.m={"n":{"item":"10.5.1","rune":"10.5.1","mastery":"10.5.1","summoner":"10.5.1","champion":"10.5.1","profileicon":"10.5.1","map":"10.5.1","language":"10.5.1","sticker":"10.5.1"},"v":"10.5.1","l":"en_US","cdn":null,"dd":"10.5.1","lg":"10.5.1","css":"10.5.1","profileiconmax":28};
