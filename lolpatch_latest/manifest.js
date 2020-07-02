97f834c2fe6d56f959214794acbfad72d058d13a
InFinity_54 <corentinkreicher@gmail.com>
2020-07-02 14:21:12 +0200

    Added patch 9.23.1

--- lolpatch_latest/manifest.js
+++ lolpatch_latest/manifest.js
@@ -1,1 +1,1 @@
-Riot.DDragon.m={"n":{"item":"9.22.1","rune":"9.22.1","mastery":"9.22.1","summoner":"9.22.1","champion":"9.22.1","profileicon":"9.22.1","map":"9.22.1","language":"9.22.1","sticker":"9.22.1"},"v":"9.22.1","l":"en_US","cdn":null,"dd":"9.22.1","lg":"9.22.1","css":"9.22.1","profileiconmax":28};
+Riot.DDragon.m={"n":{"item":"9.23.1","rune":"9.23.1","mastery":"9.23.1","summoner":"9.23.1","champion":"9.23.1","profileicon":"9.23.1","map":"9.23.1","language":"9.23.1","sticker":"9.23.1"},"v":"9.23.1","l":"en_US","cdn":null,"dd":"9.23.1","lg":"9.23.1","css":"9.23.1","profileiconmax":28};
