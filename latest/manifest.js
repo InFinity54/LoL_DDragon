bedff257dc60decaf08518e5d083320b3b616d6b
InFinity_54 <corentinkreicher@gmail.com>
2020-07-02 10:00:04 +0200

    Added patch 7.15.1

--- latest/manifest.js
+++ latest/manifest.js
@@ -1,1 +1,1 @@
-Riot.DDragon.m={"n":{"item":"7.14.1","rune":"7.14.1","mastery":"7.14.1","summoner":"7.14.1","champion":"7.14.1","profileicon":"7.14.1","map":"7.14.1","language":"7.14.1","sticker":"7.14.1"},"v":"7.14.1","l":"en_US","cdn":null,"dd":"7.14.1","lg":"7.14.1","css":"7.14.1","profileiconmax":28};
+Riot.DDragon.m={"n":{"item":"7.15.1","rune":"7.15.1","mastery":"7.15.1","summoner":"7.15.1","champion":"7.15.1","profileicon":"7.15.1","map":"7.15.1","language":"7.15.1","sticker":"7.15.1"},"v":"7.15.1","l":"en_US","cdn":null,"dd":"7.15.1","lg":"7.15.1","css":"7.15.1","profileiconmax":28};
