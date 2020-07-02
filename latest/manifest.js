579725e23627caa3048632e69a2b9aaefa541a6b
InFinity_54 <corentinkreicher@gmail.com>
2020-07-02 12:25:17 +0200

    Added patch 8.13.1

--- latest/manifest.js
+++ latest/manifest.js
@@ -1,1 +1,1 @@
-Riot.DDragon.m={"n":{"item":"8.12.1","rune":"8.12.1","mastery":"8.12.1","summoner":"8.12.1","champion":"8.12.1","profileicon":"8.12.1","map":"8.12.1","language":"8.12.1","sticker":"8.12.1"},"v":"8.12.1","l":"en_US","cdn":null,"dd":"8.12.1","lg":"8.12.1","css":"8.12.1","profileiconmax":28};
+Riot.DDragon.m={"n":{"item":"8.13.1","rune":"8.13.1","mastery":"8.13.1","summoner":"8.13.1","champion":"8.13.1","profileicon":"8.13.1","map":"8.13.1","language":"8.13.1","sticker":"8.13.1"},"v":"8.13.1","l":"en_US","cdn":null,"dd":"8.13.1","lg":"8.13.1","css":"8.13.1","profileiconmax":28};
