7683432bffb3bcd4d6345c97a54d37d936f4e40d
InFinity_54 <corentinkreicher@gmail.com>
2020-07-02 09:52:02 +0200

    Added patch 7.11.1

--- lolpatch_latest/manifest.js
+++ lolpatch_latest/manifest.js
@@ -1,1 +1,1 @@
-Riot.DDragon.m={"n":{"item":"7.10.1","rune":"7.10.1","mastery":"7.10.1","summoner":"7.10.1","champion":"7.10.1","profileicon":"7.10.1","map":"7.10.1","language":"7.10.1","sticker":"7.10.1"},"v":"7.10.1","l":"en_US","cdn":null,"dd":"7.10.1","lg":"7.10.1","css":"7.10.1","profileiconmax":28};
+Riot.DDragon.m={"n":{"item":"7.11.1","rune":"7.11.1","mastery":"7.11.1","summoner":"7.11.1","champion":"7.11.1","profileicon":"7.11.1","map":"7.11.1","language":"7.11.1","sticker":"7.11.1"},"v":"7.11.1","l":"en_US","cdn":null,"dd":"7.11.1","lg":"7.11.1","css":"7.11.1","profileiconmax":28};
