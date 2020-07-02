b3d4bd549f29a426def3a560856063aa1b295499
InFinity_54 <corentinkreicher@gmail.com>
2020-07-02 11:30:59 +0200

    Added patch 7.24.1

--- lolpatch_latest/manifest.js
+++ lolpatch_latest/manifest.js
@@ -1,1 +1,1 @@
-Riot.DDragon.m={"n":{"item":"7.23.1","rune":"7.23.1","mastery":"7.23.1","summoner":"7.23.1","champion":"7.23.1","profileicon":"7.23.1","map":"7.23.1","language":"7.23.1","sticker":"7.23.1"},"v":"7.23.1","l":"en_US","cdn":null,"dd":"7.23.1","lg":"7.23.1","css":"7.23.1","profileiconmax":28};
+Riot.DDragon.m={"n":{"item":"7.24.1","rune":"7.24.1","mastery":"7.24.1","summoner":"7.24.1","champion":"7.24.1","profileicon":"7.24.1","map":"7.24.1","language":"7.24.1","sticker":"7.24.1"},"v":"7.24.1","l":"en_US","cdn":null,"dd":"7.24.1","lg":"7.24.1","css":"7.24.1","profileiconmax":28};
