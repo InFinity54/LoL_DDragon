c2e8f6aa013ef7b006f5e91f1a9ee107170de7fc
InFinity_54 <corentinkreicher@gmail.com>
2020-07-02 13:01:35 +0200

    Added patch 9.2.1

--- latest/manifest.js
+++ latest/manifest.js
@@ -1,1 +1,1 @@
-Riot.DDragon.m={"n":{"item":"9.1.1","rune":"9.1.1","mastery":"9.1.1","summoner":"9.1.1","champion":"9.1.1","profileicon":"9.1.1","map":"9.1.1","language":"9.1.1","sticker":"9.1.1"},"v":"9.1.1","l":"en_US","cdn":null,"dd":"9.1.1","lg":"9.1.1","css":"9.1.1","profileiconmax":28};
+Riot.DDragon.m={"n":{"item":"9.2.1","rune":"9.2.1","mastery":"9.2.1","summoner":"9.2.1","champion":"9.2.1","profileicon":"9.2.1","map":"9.2.1","language":"9.2.1","sticker":"9.2.1"},"v":"9.2.1","l":"en_US","cdn":null,"dd":"9.2.1","lg":"9.2.1","css":"9.2.1","profileiconmax":28};
