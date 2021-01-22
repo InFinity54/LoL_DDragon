0a715dc9260dc69cf2373bd1de068d3117507d18
InFinity_54 <corentinkreicher@gmail.com>
2021-01-22 19:23:29 +0100

    Added patch 11.2.1

--- README.md
+++ README.md
@@ -46,4 +46,5 @@ This list contains patchs from actual season. For all previous patchs of all pre
 
 The date in front of each patch represents the date when the patch was pushed to this repository, not the date when it was released by Riot Games. Here's a list of all patchs of current seasons included in this repository :
 
+- (January 22th, 2021) 11.2.1
 - (January 5th, 2021) 11.1.1
--- latest/manifest.js
+++ latest/manifest.js
@@ -1,1 +1,1 @@
-Riot.DDragon.m={"n":{"item":"11.1.1","rune":"11.1.1","mastery":"11.1.1","summoner":"11.1.1","champion":"11.1.1","profileicon":"11.1.1","map":"11.1.1","language":"11.1.1","sticker":"11.1.1"},"v":"11.1.1","l":"en_US","cdn":null,"dd":"11.1.1","lg":"11.1.1","css":"11.1.1","profileiconmax":28};
+Riot.DDragon.m={"n":{"item":"11.2.1","rune":"11.2.1","mastery":"11.2.1","summoner":"11.2.1","champion":"11.2.1","profileicon":"11.2.1","map":"11.2.1","language":"11.2.1","sticker":"11.2.1"},"v":"11.2.1","l":"en_US","cdn":null,"dd":"11.2.1","lg":"11.2.1","css":"11.2.1","profileiconmax":28};
