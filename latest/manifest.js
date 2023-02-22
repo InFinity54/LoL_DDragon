55a949e8b750fc03d6f10b3b4855a5f328a86fcd
InFinity_54 <corentinkreicher@gmail.com>
2023-02-22 22:17:53 +0100

    Added patch 13.4.1

--- latest/manifest.js
+++ latest/manifest.js
@@ -1,1 +1,1 @@
-Riot.DDragon.m={"n":{"item":"13.3.1","rune":"13.3.1","mastery":"13.3.1","summoner":"13.3.1","champion":"13.3.1","profileicon":"13.3.1","map":"13.3.1","language":"13.3.1","sticker":"13.3.1"},"v":"13.3.1","l":"en_US","cdn":null,"dd":"13.3.1","lg":"13.3.1","css":"13.3.1","profileiconmax":28};
+Riot.DDragon.m={"n":{"item":"13.4.1","rune":"13.4.1","mastery":"13.4.1","summoner":"13.4.1","champion":"13.4.1","profileicon":"13.4.1","map":"13.4.1","language":"13.4.1","sticker":"13.4.1"},"v":"13.4.1","l":"en_US","cdn":null,"dd":"13.4.1","lg":"13.4.1","css":"13.4.1","profileiconmax":28};
